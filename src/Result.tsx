754ce152d791a75d0b1f656ed93dc9ef9c37c86b
Tim Raderschad <tim.raderschad@gmail.com>
2021-03-18 20:14:18 +0100

    fix: fix ci

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@chakra-ui/button'
 import { Input, InputGroup, InputLeftAddon } from '@chakra-ui/input'
 import { Box, Flex } from '@chakra-ui/layout'
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 interface Props {
   spec: Fig.Spec | null
 }
@@ -10,25 +10,24 @@ function parseInputValue(value) {
   return value.split(' ')
 }
 
-
 function getSuggestions(input: string[], spec: Fig.Spec | null) {
-  let suggs: string[] = [];
-  if (spec == null) return [];
-  for (let i = 0 ; i < input.length; i++) {
-    const bit = input[i];
+  let suggs: string[] = []
+  if (spec == null) return []
+  for (let i = 0; i < input.length; i++) {
+    const bit = input[i]
     if (i === 0) {
       // command
       const command = spec.name
       if (command.indexOf(bit) === 0) {
         suggs.push(command)
       } else {
-        break;
+        break
       }
     } else {
       suggs.splice(0, suggs.length)
       // every other possibility (subcommands)
-      const {subcommands, args, options} = spec;
-      subcommands?.forEach(({name: subcommandName}) => {
+      const { subcommands } = spec
+      subcommands?.forEach(({ name: subcommandName }) => {
         if (subcommandName.indexOf(bit) === 0) {
           suggs.push(subcommandName as string)
         }
@@ -42,21 +41,40 @@ const Result = ({ spec }: Props) => {
   const [input, setInput] = useState<string[]>([])
   const [suggs, setSuggs] = useState<string[]>([])
 
-  useEffect(()=> {
+  useEffect(() => {
     setSuggs(getSuggestions(input, spec))
-  },[input, spec])
+  }, [input, spec])
 
   return (
-    <Box pos="relative" background="gray.900" height="full" textAlign="left">      
+    <Box pos="relative" background="gray.900" height="full" textAlign="left">
       <InputGroup>
-        <InputLeftAddon p="sm" fontFamily="monospace" color="orange.600" bg="transparent" border="none" pointerEvents="none" children="fig@web $"></InputLeftAddon>
-        <Input onChange={(e) => setInput(parseInputValue(e.currentTarget.value))} fontSize="sm" border="none" color="white" autoFocus _focus={{outline: "none"}} />
+        <InputLeftAddon
+          p="sm"
+          fontFamily="monospace"
+          color="orange.600"
+          bg="transparent"
+          border="none"
+          pointerEvents="none"
+          children="fig@web $"
+        ></InputLeftAddon>
+        <Input
+          onChange={(e) => setInput(parseInputValue(e.currentTarget.value))}
+          fontSize="sm"
+          border="none"
+          color="white"
+          autoFocus
+          _focus={{ outline: 'none' }}
+        />
       </InputGroup>
-      {suggs.length > 0 &&              
+      {suggs.length > 0 && (
         <Flex justifyContent="left" direction="column" pos="absolute">
-          {suggs.map((sugg, index) => (<Button textAlign="left" variant="link">{sugg}</Button>))}
+          {suggs.map((sugg, index) => (
+            <Button textAlign="left" variant="link">
+              {sugg}
+            </Button>
+          ))}
         </Flex>
-      }
+      )}
     </Box>
   )
 }
